0e8b141b512cb48210828ccc41e93ed1e3f8f91b
Ben Kloosterman <bklooste@gmail.com>
2019-09-01 22:06:20 +1000

    fixed eventstore projections

--- inventory-logic/setup-integration-test-dependencies.cmd
+++ inventory-logic/setup-integration-test-dependencies.cmd
@@ -1,6 +1,7 @@
-docker run --name eventstore-node -dt -p 2113:2113 -p 1113:1113 eventstore/eventstore
+docker run -d --name eventstore-node -p 2113:2113 -p 1113:1113 -e EVENTSTORE_RUN_PROJECTIONS=ALL -e EVENTSTORE_START_STANDARD_PROJECTIONS=TRUE eventstore/eventstore
+
 docker start eventstore-node
 REM below requires latest eg release build
-docker run -dt -e "DOTNET_USE_POLLING_FILE_WATCHER=1" -p 53104:80 --entrypoint tail simplecqrsapi:latest
+docker run -d -e "DOTNET_USE_POLLING_FILE_WATCHER=1" -e "ConnectionStrings:EventStoreConnection=ConnectTo=tcp://admin:changeit@host.docker.internal:1113;HeartBeatTimeout=500" -p 53104:80 simplecqrsapi:latest
 REM docker run -dt -e "DOTNET_USE_POLLING_FILE_WATCHER=1" -p 53105:80 --entrypoint tail simplecqrs-view:dev -f /dev/null
 timeout 15
